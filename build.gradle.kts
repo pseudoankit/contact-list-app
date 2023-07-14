ca665d1dbb5061da2ec977e512a9610bfd8f808e
ankitkumar <lostankit7@gmail.com>
2023-07-14 08:11:49 +0530

    dependecny setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,15 @@
+buildscript {
+    dependencies {
+        classpath("com.squareup.sqldelight:gradle-plugin:1.5.5")
+    }
+}
+
 plugins {
     //trick: for the same plugin versions in all sub-modules
-    id("com.android.application").version("8.0.1").apply(false)
-    id("com.android.library").version("8.0.1").apply(false)
-    kotlin("android").version("1.8.10").apply(false)
-    kotlin("multiplatform").version("1.8.10").apply(false)
+    kotlin("multiplatform").apply(false)
+    id("com.android.application").apply(false)
+    id("com.android.library").apply(false)
+    id("org.jetbrains.compose").apply(false)
 }
 
 tasks.register("clean", Delete::class) {
